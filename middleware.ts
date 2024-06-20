5da4adb567e79071985a030d90b2ae37fd093b80
Reza Pazan <rez.paz78@gmail.com>
2024-06-20 20:01:24 +0330

    DELETE(middleware): test codes

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
 import { stackMiddlewares } from '@/middlewares/stackMiddlewares'
-import { withAuth } from '@/middlewares/withAuth'
 
-const middlewares = [withAuth]
-export default stackMiddlewares(middlewares)
+const middlewares = []
+// export default stackMiddlewares(middlewares)
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
